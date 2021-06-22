903d3e9de3638df9397cb045b5bb57352e0dc0f1
5CYTH3 <ScytheD@outlook.fr>
2021-06-22 10:14:43 +0200

    🎎 Added a new button in the window. Set the scene not resizable.

--- src/main/java/fr/scythe/alephmachine/Main.java
+++ src/main/java/fr/scythe/alephmachine/Main.java
@@ -5,8 +5,15 @@
 package fr.scythe.alephmachine;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
 import javafx.scene.control.Button;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -24,16 +31,39 @@ public class Main extends Application {
     public void start(Stage pStage) throws Exception {
         System.out.println("Launch of the app. . .");
         pStage.setTitle("Aleph Machine");
+        pStage.setResizable(false);
+
+
 
         // Construct of the button
         Button btnStart = new Button();
         btnStart.setText("Start Aleph");
+        btnStart.setBackground(new Background(new BackgroundFill(Color.grayRgb(170), new CornerRadii(5), Insets.EMPTY)));
+
+        // onClick event
+        btnStart.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Calling botInit() method. . .");
+                botInit();
+            }
+        });
 
         // Adding of the stack pane (Container)
         StackPane stackPane = new StackPane();
         stackPane.getChildren().add(btnStart);
 
-        // Scene = the window
-        Scene scene = new Scene(stackPane, 300, 400);
+        // pScene = the window
+        Scene pScene = new Scene(stackPane, 300, 400);
+        pStage.setScene(pScene);
+        pStage.show();
+
+
     }
+
+    // Launch of the bot
+    public static void botInit() {
+
+    }
+
 }
